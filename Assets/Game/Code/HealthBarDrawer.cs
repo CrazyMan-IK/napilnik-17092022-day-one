9b3b54a2646d0127d20a1bcc85731fc913727a16
Даниэль Давлетшин <Superdanik2004@gmail.com>
2022-09-17 18:44:05 +0500

    Almost made health bar system

---
+++ Assets/Game/Code/HealthBarDrawer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DayOne
+{
+    public class HealthBarDrawer : MonoBehaviour
+    {
+        [SerializeField] private Mesh _mesh = null;
+        [SerializeField] private Material _material = null;
+
+        private Matrix4x4[] _transforms = null;
+        private float[] _values = null;
+        private List<Health> _targets = null;
+
+        private void Start()
+        {
+            _targets = FindObjectsOfType<Health>().ToList();
+
+            UpdateArrays();
+        }
+
+        private void Update()
+        {
+            var isAnyDied = false;
+            for (int i = 0; i < _targets.Count; i++)
+            {
+                var target = _targets[i];
+
+                _values[i] = target.Value;
+                _transforms[i] = Matrix4x4.Translate(target.transform.position); //target.transform.localToWorldMatrix;
+
+                if (target.Value <= 0)
+                {
+                    isAnyDied = true;
+
+                    _targets.Remove(target);
+                    i--;
+                }
+            }
+
+            if (isAnyDied)
+            {
+                UpdateArrays();
+            }
+
+            _material.SetFloatArray("_Values", _values);
+
+            Graphics.DrawMeshInstanced(_mesh, 0, _material, _transforms);
+        }
+
+        private void UpdateArrays()
+        {
+            _values = new float[_targets.Count];
+            _transforms = new Matrix4x4[_targets.Count];
+
+            for (int i = 0; i < _targets.Count; i++)
+            {
+                var target = _targets[i];
+
+                _values[i] = target.Value;
+                _transforms[i] = target.transform.localToWorldMatrix;
+            }
+        }
+    }
+}
---
+++ Assets/Game/Shader/HealthBar.shader
@@ -0,0 +1,69 @@
+// Upgrade NOTE: upgraded instancing buffer 'Props' to new syntax.
+
+Shader "Unlit/HealthBar"
+{
+	Properties
+	{
+
+	}
+		SubShader
+	{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" "DisableBatching" = "True" }
+
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_instancing
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : POSITION;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float value : TEXCOORD1;
+				float4 pos : SV_POSITION;
+			};
+
+			float _Values[1023];
+
+			v2f vert(appdata v, uint instanceID: SV_InstanceID)
+			{
+				// Allow instancing.
+				UNITY_SETUP_INSTANCE_ID(v);
+
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv.xy;
+
+				// billboard mesh towards camera
+				float3 vpos = mul((float3x3)unity_ObjectToWorld, v.vertex.xyz) * float3(2, 0.3, 1);
+				float4 worldCoord = float4(unity_ObjectToWorld._m03, unity_ObjectToWorld._m13, unity_ObjectToWorld._m23, 1) + float4(0, 1, 0, 0);
+				float4 viewPos = mul(UNITY_MATRIX_V, worldCoord) + float4(vpos, 0);
+				float4 outPos = mul(UNITY_MATRIX_P, viewPos);
+
+				o.pos = outPos;
+				o.value = _Values[instanceID];
+
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				return fixed4(i.value, 0, 0, 1.0);
+			}
+			ENDCG
+		}
+	}
+}
